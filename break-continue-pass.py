1f7cb1ec7af2f6e42b09461929f3e62cfdb4221d
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-26 20:14:08 -0600

    Add files via upload

---
+++ break-continue-pass.py
@@ -0,0 +1,84 @@
+# example
+my_list = [1,2,3]
+for item in my_list:
+    print(item)
+
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    break
+
+# example
+my_list = [1,2,3]
+for item in my_list:
+    print(item)
+    break
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    break
+
+# example
+my_list = [1,2,3]
+for item in my_list:
+    print(item)
+    continue
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    continue
+
+# example
+my_list = [1,2,3]
+for item in my_list:
+    continue
+    print(item)
+        
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    continue
+    print(my_list[i])
+
+# example pass doesn't do anything
+my_list = [1,2,3]
+for item in my_list:
+    pass
+    print(item)
+        
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    pass
+
+# example pass doesn't do anything
+my_list = [1,2,3]
+for item in my_list:
+    # thinking about it (pass release and let go)
+    pass
+        
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    pass
+
+
+
+
+
+
+
+
+ 
